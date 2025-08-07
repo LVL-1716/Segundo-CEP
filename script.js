fcf3b95f32603c040833cff0d4e67ee2b2c8bb71
Luciano <lvlbrasil@gmail.com>
2025-08-07 12:45:10 -0300

    Atualiza01

--- script.js
+++ script.js
@@ -5,8 +5,16 @@
     const bairro = document.getElementById('bairro').value;
     const cidade = document.getElementById('cidade').value;
     const uf = document.getElementById('uf').value;
-    
 
+    // Cria um objeto com os dados dos campos
+  const dados = {
+    cep: cep,
+    rua: rua,
+    bairro: bairro,
+    cidade: cidade,
+    uf: uf
+  };
+    
     localStorage.setItem('salvarDados', JSON.stringify(dados));
     alert('Dados salvos com sucesso!');
   }
